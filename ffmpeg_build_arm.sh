b0cc92af957da72d2c3d1c6cacf6afc18716bff8
yousirui1 <yousirui1@163.com>
2019-03-30 15:12:10 +0800

    ffmpeg build sh

---
+++ ffmpeg_build_arm.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+./configure --prefix=build/arm \
+  --enable-static   \
+  --enable-shared   \
+  --disable-everything \
+  --enable-decoder=h264 \
+  --enable-encoder=h264  \
+  --enable-gpl   \
+  --enable-version3  \
+  --enable-pthreads \
+  --disable-asm   \
+  --disable-vaapi  \
+  --disable-vdpau  \
+  --enable-muxer=h264 \
+  --enable-parser=h264 \
+  --enable-demuxer=h264 \
+  --enable-protocal=file \
+  --cross-prefix = arm_linux-gnueabihf- \
+  --arch=arm      \
+  --target-os = linux 
